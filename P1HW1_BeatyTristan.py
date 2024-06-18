5cf13bda6ff98702c745e675259a9ffe54452c53
HOITMAN <143423255+HOITMAN@users.noreply.github.com>
2024-06-18 13:45:14 -0400

    Add files via upload

---
+++ P1HW1_BeatyTristan.py
@@ -0,0 +1,16 @@
+#Tristan Beaty
+#6/18/2004
+#P1HW1
+#sampledisc
+
+print('-----Calculating Exponents-----\n\n')
+
+print('Enter an integer as the base value:', end = ' ')
+base = input()
+
+print('Enter an integer as the exponent', end = ' ')
+exponent = input() 
+
+print()
+
+      
