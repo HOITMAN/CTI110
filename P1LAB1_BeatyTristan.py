7fd5686336e72149f4112425704c25f21f738b55
HOITMAN <143423255+HOITMAN@users.noreply.github.com>
2024-06-18 13:17:51 -0400

    Add files via upload

---
+++ P1LAB1_BeatyTristan.py
@@ -0,0 +1,12 @@
+#Tristan Beaty
+#6/18/2024
+#P1LAB1
+#Get inputs and display outputs
+
+print('Enter your first name:', end = ' ')
+first_name = input()
+
+print('Enter your last name:', end = ' ')
+last_name = input()
+
+print('Hello,', first_name, last_name+'! Welcome to CTI-110')
