4eeca1595830ca168bf9f77b89e638f62a0f2d82
HOITMAN <143423255+HOITMAN@users.noreply.github.com>
2024-06-20 13:51:36 -0400

    Add files via upload

--- P1HW1_BeatyTristan.py
+++ P1HW1_BeatyTristan.py
@@ -1,16 +1,34 @@
 #Tristan Beaty
 #6/18/2004
 #P1HW1
-#sampledisc
+#Obtaining inputs to preform mathmatical equations
 
-print('-----Calculating Exponents-----\n\n')
+print('-----Calculating Exponents-----\n')
 
 print('Enter an integer as the base value:', end = ' ')
-base = input()
+base_exp = int(input())
 
 print('Enter an integer as the exponent', end = ' ')
-exponent = input() 
-
+exponent = int(input())
 print()
+
+print(base_exp, 'to the power of', exponent, 'is', base_exp ** exponent, '\n')
+
+
+print('-----Addition and Subtraction-----\n')
+ 
+print('Enter a starting value:', end=' ')
+base_adsu = int(input())
+
+print('Enter an integer to add:', end = ' ')
+num_add = int(input())
+
+print('Enter an integer to subtract', end = ' ')
+num_subtract = int(input())
+print()
+
+print(base_adsu, '+', num_add, '-', num_subtract, 'is equal to', base_adsu + num_add - num_subtract, '\n\n')
+
+input('Press ENTER to close')
 
       
